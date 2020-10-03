342ffdb2462b9da3006127e74ad435c5d05934fc
iqbal125 <iqbal125@yahoo.com>
2020-10-02 18:03:52 -0700

    fix css loader

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require("webpack")
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+ {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { 
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: "[name]__[local]___[hash:base64:5]",
+                            },														
+                            sourceMap: true
+                        }
+                     },
+                     { 
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [ 'autoprefixer', {}, ],
+                                ],
+                            },
+                        }
+                      }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=10000&name=img/[name].[ext]'
+            }
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+}
