227fe34b9ed918ecdc4265ada35a5a9be1357d1d
iqbal125 <iqbal125@yahoo.com>
2021-02-23 11:43:20 -0800

    fix env name

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import "./App.css"
 function App() {
   //Add cdk
 
-  console.log("FFFFFFFFFFFFF" + process.env.key1)
+  console.log("FFFFFFFFFFFFF" + process.env.REACT_APP_key1)
 
   return (
     <div className="App">
@@ -17,7 +17,7 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-        {process.env.key1 ? <p>{process.env.key1}</p> : <p>No key Found</p>}
+        {process.env.REACT_APP_key1 ? <p>{process.env.REACT_APP_key1}</p> : <p>No key Found</p>}
       </header>
     </div>
   )
